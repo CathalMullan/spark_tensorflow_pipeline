3bd06ddb5aae241a3c3eaa7953f0f1e8df49555f
Cathal Mullan <cmullan@proofpoint.com>
2019-10-19 21:10:22 +0100

    Add script to init venv. Use Poetry for python dependency and project management.

---
+++ Makefile
@@ -0,0 +1,3 @@
+.PHONY: venv
+venv:
+	./scripts/init_environment.sh
---
+++ scripts/init_environment.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+: <<'COMMENT'
+Create the virtual environment (venv) for Python development.
+Install packages using Poetry,
+COMMENT
+
+# Ensure Python 3.7.* is installed and active
+REQUIRED_PYTHON=3.7
+
+PYTHON_VERSION="$(python --version 2>&1)"
+case $PYTHON_VERSION in
+    *"$REQUIRED_PYTHON"*)
+        ;;
+    *)
+        echo "Incorrect Python version - project requires Python $REQUIRED_PYTHON, found $PYTHON_VERSION"
+        echo "Recommended installation - https://github.com/pyenv/pyenv"
+        exit 1
+esac
+
+
+# cd to project root
+cd "$(dirname "${0}")" || exit
+cd ../
+
+# Create venv
+python -m venv venv
+source venv/bin/activate
+
+# Update pip and install poetry
+pip install --upgrade pip
+pip install poetry
+
+# Install packages
+poetry install
